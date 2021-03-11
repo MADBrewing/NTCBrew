c4b51eb0d7a8b0f8844a81efb3567590cac3da07
Vojtech Hrdina <vhrdina@gmail.com>
2021-03-11 21:43:29 +0000

    wip - add translations

--- lib/ui/formulas/FormWidgetProvider.dart
+++ lib/ui/formulas/FormWidgetProvider.dart
@@ -1,42 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:ntcbrew/ui/formulas/FormulaController.dart';
+import 'package:ntcbrew/utils/Strings.dart';
 
 class FormWidgetProvider {
   Input og(BuildContext context, {action = TextInputAction.next}) =>
-      Input("OG", FormulaInputType.OG, action: action);
+      Input(getString(context, (res) => res?.og_text_field_label), FormulaInputType.OG, action: action);
 
   Input fg(BuildContext context, {action = TextInputAction.next}) =>
-      Input("FG", FormulaInputType.FG, action: action);
+      Input(getString(context, (res) => res?.fg_text_field_label), FormulaInputType.FG, action: action);
 
   Input batchSize(BuildContext context, {action = TextInputAction.next}) =>
-      Input("Batch Size", FormulaInputType.BATCH_SIZE, action: action);
+      Input(getString(context, (res) => res?.batch_size_text_field_label), FormulaInputType.BATCH_SIZE, action: action);
 
   Input plato(BuildContext context, {action = TextInputAction.next}) =>
-      Input("Plato", FormulaInputType.PLATO, action: action);
+      Input(getString(context, (res) => res?.plato_text_field_label), FormulaInputType.PLATO, action: action);
 
   Input brixStart(BuildContext context, {action = TextInputAction.next}) =>
-      Input("Brix Start", FormulaInputType.BRIX_START, action: action);
+      Input(getString(context, (res) => res?.brix_start_text_field_label), FormulaInputType.BRIX_START, action: action);
 
   Input brixEnd(BuildContext context, {action = TextInputAction.next}) =>
-      Input("Brix End", FormulaInputType.BRIX_END, action: action);
+      Input(getString(context, (res) => res?.brix_end_text_field_label), FormulaInputType.BRIX_END, action: action);
 
   Input weightOfMalt(BuildContext context, {action = TextInputAction.next}) =>
-      Input("Weight of Malt", FormulaInputType.WEIGHT_OF_MALT, action: action);
+      Input(getString(context, (res) => res?.weight_of_malt_text_field_label), FormulaInputType.WEIGHT_OF_MALT, action: action);
 
   Input avgMaltEBC(BuildContext context, {action = TextInputAction.next}) =>
-      Input("Average Malt EBC", FormulaInputType.AVG_MALT_EBC, action: action);
+      Input(getString(context, (res) => res?.avg_malt_ebc_field_label), FormulaInputType.AVG_MALT_EBC, action: action);
 
   Input avgExtr(BuildContext context, {action = TextInputAction.next}) =>
-      Input("Average Extr", FormulaInputType.AVG_EXTR, action: action);
+      Input(getString(context, (res) => res?.avg_extr_text_field_label), FormulaInputType.AVG_EXTR, action: action);
 
   Input attenuation(BuildContext context, {action = TextInputAction.next}) =>
-      Input("Attenuation", FormulaInputType.ATTENUATION, action: action);
+      Input(getString(context, (res) => res?.attenuation_text_field_label), FormulaInputType.ATTENUATION, action: action);
 
   Input epm(BuildContext context, {action = TextInputAction.next}) =>
-      Input("Epm", FormulaInputType.EPM, action: action);
+      Input(getString(context, (res) => res?.epm_text_field_label), FormulaInputType.EPM, action: action);
 
   Input sg(BuildContext context, {action = TextInputAction.next}) =>
-      Input("Sg", FormulaInputType.SG, action: action);
+      Input(getString(context, (res) => res?.sg_text_field_label), FormulaInputType.SG, action: action);
 }
 
 enum FormulaInputType { OG, FG, BATCH_SIZE, PLATO, BRIX_START, BRIX_END, WEIGHT_OF_MALT, AVG_MALT_EBC, AVG_EXTR, ATTENUATION, EPM, SG }
